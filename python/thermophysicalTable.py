d1ee747e019186d903553ddeda7eed3b0fbbf62d
Cyril Levet <cyril.levet0780@orange.fr>
2018-05-13 15:25:18 +0200

    Add licencing information in thermophysicalTable.py

--- python/thermophysicalTable.py
+++ python/thermophysicalTable.py
@@ -1,6 +1,30 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+################################################################################
+'''
+    Copyright (C) 2018 Yuusha
+'''
+################################################################################
+'''
+License
+    This file is part of Yuusha contribution to OpenFOAM.
+
+    This contribution is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This contribution is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+    See the GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this contribution.  If not, see <http://www.gnu.org/licenses/>.
+'''
+################################################################################
+
 from re import sub
 
 class thermophysicalTable() :
